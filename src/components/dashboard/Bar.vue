40e19d7081b23d40adf1974a2eae4c236b4fa7b4
themustafaomar <themustafaomar@gmail.com>
2023-04-26 02:29:21 +0200

    feat(dashboard): add dashboard components

---
+++ src/components/dashboard/Bar.vue
@@ -0,0 +1,42 @@
+<template>
+  <v-app-bar elevation="0" prominent>
+    <v-app-bar-nav-icon variant="text" @click.stop="$emit('rail:switch')" />
+
+    <v-toolbar-title class="text-uppercase text-grey-darken-1">Wakeb</v-toolbar-title>
+
+    <v-text-field
+      density="compact"
+      placeholder="Search term..."
+      prepend-inner-icon="mdi-magnify"
+      class="rounded-pill mt-5" />
+
+    <v-spacer></v-spacer>
+
+    <template #append>
+      <v-btn variant="text">Home</v-btn>
+
+      <v-btn variant="text">Blog</v-btn>
+
+      <v-btn variant="text">Other</v-btn>
+
+      <v-divider class="mx-2 my-auto" inset vertical style="height: 16px" />
+
+      <theme-toggle />
+
+      <v-divider class="mx-2 my-auto" inset vertical style="height: 16px" />
+
+      <app-bar-notifications-menu />
+
+      <app-bar-language-menu />
+
+      <app-settings-menu />
+    </template>
+  </v-app-bar>
+</template>
+
+<script setup>
+import ThemeToggle from '../app/common/ThemeToggle.vue'
+import AppBarNotificationsMenu from '../app/bar/NotificationsMenu.vue'
+import AppBarLanguageMenu from '../app/bar/LanguageMenu.vue'
+import AppSettingsMenu from '../app/bar/SettingsMenu.vue'
+</script>
---
+++ src/components/dashboard/Heading.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-sheet class="d-flex align-center justify-space-between pa-5" rounded="lg">
+    <slot v-if="$slots.hi" name="hi"></slot>
+    <div v-else>
+      <h2 class="font-weight-medium text-h5">{{ title }}</h2>
+      <p class="text-medium-emphasis">{{ description }}</p>
+    </div>
+    <slot name="action"></slot>
+  </v-sheet>
+</template>
+
+<script setup>
+import { defineProps } from 'vue'
+
+defineProps({
+  title: String,
+  description: String,
+})
+</script>
