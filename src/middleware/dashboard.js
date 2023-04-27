8b2eba3a11170ae60751a3e616b830fec058a4fa
Mustafa Omar <themustafaomar@gmail.com>
2023-04-27 14:16:50 +0200

    fix: middlewares

--- src/middleware/dashboard.js
+++ src/middleware/dashboard.js
@@ -1,4 +1,18 @@
 /* eslint-disable */
-export default ({ guest, from, to, next }) => {
-  //
+export default ({ store, router, from, to, next, guest }) => {
+  const isLoggedIn = store.getters['auth/isLoggedIn']
+
+  // This is a guest route, we want
+  // to redirect this user to the home.
+  if (guest && isLoggedIn) {
+    return router.push(store.state.app.auth.homeURL)
+  }
+
+  // This is an authenticated route
+  // but the user is not logged in so redirect.
+  if (!guest && !isLoggedIn) {
+    return router.push({ name: 'login' })
+  }
+
+  next()
 }
