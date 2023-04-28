d789c24995758a5acc1301bcedd904417d49e181
Mustafa Omar <themustafaomar@gmail.com>
2023-04-28 06:45:17 +0200

    feat(dialogs): add customizable dialogs

---
+++ src/components/common/Dialog.vue
@@ -0,0 +1,48 @@
+<template>
+  <v-dialog v-model="dialog" width="600px">
+    <v-card rounded="lg">
+      <v-card-title v-if="!$slots.title" class="border-b">
+        <h3 class="font-weight-regular text-h6 mb-2">
+          {{ title || 'Are you sure you want to delete this item?' }}
+        </h3>
+      </v-card-title>
+      <slot v-else name="title">No title set</slot>
+
+      <v-card-text v-if="!$slots.message">
+        <p class="text-medium-emphasis">
+          {{ message }}
+        </p>
+      </v-card-text>
+      <slot v-else></slot>
+
+      <v-card-actions class="px-4 pb-4">
+        <template v-if="!$slots.actions">
+          <v-btn color="red" @click="dialog = false">Close</v-btn>
+        </template>
+        <slot v-else name="actions" v-bind="{ close: () => (dialog = false), payload }"></slot>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+const dialog = ref(false)
+const payload = ref(null)
+
+function show(data) {
+  dialog.value = true
+
+  if (typeof payload !== 'undefined') {
+    payload.value = data
+  }
+}
+
+function hide() {
+  dialog.value = false
+}
+
+defineProps({ title: String, message: String })
+defineExpose({ show, hide, dialog })
+</script>
--- src/pages/dashboard/index.vue
+++ src/pages/dashboard/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <AppDashboardHeading
+  <app-dashboard-heading
     title="Dashboard"
     description="Welcome to dashboard, it's nice to see you again!"
   />
@@ -8,20 +8,18 @@
   <v-row v-for="n in 1" :key="n" class="mt-2">
     <v-col v-for="n in 3" cols="4" :key="n">
       <v-sheet class="pa-5" rounded="lg">
-        <h3 class="font-weight-regular text-h6 mb-2">This is the title number {{ n }}</h3>
+        <h3 class="font-weight-regular text-h6 mb-2">Grid system box {{ n }}</h3>
         <p class="text-medium-emphasis">
           Lorem ipsum elit, placeat officiis nam ullam deleniti accusantium alias tempore velit
           veniam vero sapiente, aperiam at!
         </p>
-        <v-btn color="primary" elevation="0" rounded="pill" class="mt-4">
-          Learn more
-          <v-icon class="ms-1">mdi-arrow-right</v-icon>
-        </v-btn>
       </v-sheet>
     </v-col>
+  </v-row>
 
+  <v-row class="mb-5">
     <!-- toasts (snackbars) -->
-    <v-col>
+    <v-col cols="4">
       <v-sheet class="pa-5" rounded="lg">
         <h3 class="font-weight-regular text-h6 mb-2">
           This is to test
@@ -43,36 +41,211 @@
     </v-col>
 
     <!-- delete dialogs -->
-    <v-col>
+    <v-col cols="4">
       <v-sheet class="pa-5" rounded="lg">
         <h3 class="font-weight-regular text-h6 mb-2">
           This is to test
           <code>dialogs</code>
         </h3>
         <p class="text-medium-emphasis">
-          Lorem ipsum elit, placeat officiis nam ullam deleniti accusantium alias tempore velit
-          veniam vero sapiente, aperiam at!
+          Are you sure you want to delete this item? this item will be deleted immediately. you
+          cannot undo this action.
         </p>
-        <v-btn @click.prevent color="primary" elevation="0" rounded="pill" class="mt-4">
+        <v-btn @click="dialog.show(10)" color="primary" elevation="0" rounded="pill" class="mt-4">
           Show delete dialog
           <v-icon class="ms-1">mdi-delete-outline</v-icon>
         </v-btn>
       </v-sheet>
     </v-col>
+
+    <!-- error page simulation -->
+    <v-col cols="4">
+      <v-sheet class="pa-5" rounded="lg">
+        <h3 class="font-weight-regular text-h6 mb-2">
+          This is to test
+          <code>error</code>
+          page
+        </h3>
+        <p class="text-medium-emphasis">
+          Lorem ipsum elit, placeat officiis nam ullam deleniti accusantium alias tempore velit
+          veniam vero sapiente, aperiam at!
+        </p>
+        <v-btn @click.prevent="makeError" color="red" elevation="0" rounded="pill" class="mt-4">
+          Simulate error
+          <v-icon class="ms-1">mdi-delete-outline</v-icon>
+        </v-btn>
+      </v-sheet>
+    </v-col>
+
+    <!-- text fields -->
+    <v-col cols="3">
+      <v-sheet class="pa-5" rounded="lg">
+        <h3 class="font-weight-regular text-h6 mb-2">
+          This is to test
+          <code>text fields</code>
+        </h3>
+        <p class="text-medium-emphasis mb-6">This is an example of using the custom text fields.</p>
+
+        <app-text-field
+          v-model="form.name"
+          :form="form"
+          name="name"
+          label="Name"
+          placeholder="Your Name"
+          persistent-placeholder
+          required
+          class="mb-4"
+        ></app-text-field>
+
+        <app-text-field
+          v-model="form.email"
+          :form="form"
+          name="email"
+          label="Email"
+          placeholder="Your Email"
+          persistent-placeholder
+          required
+          class="mb-4"
+        ></app-text-field>
+
+        <app-text-field
+          v-model="form.email"
+          :form="form"
+          name="email"
+          label="Email"
+          placeholder="Your Email"
+          persistent-placeholder
+          required
+          class="mb-4"
+        ></app-text-field>
+
+        <app-text-field
+          v-model="form.password"
+          :form="form"
+          name="password"
+          type="password"
+          label="Password"
+          placeholder="Your Password"
+          persistent-placeholder
+          required
+          class="mb-4"
+        ></app-text-field>
+
+        <v-btn @click="sendRequest" size="large" block color="primary" elevation="0">
+          <v-progress-circular
+            v-if="form.busy"
+            color="white"
+            bg-color="transparent"
+            indeterminate
+            size="27"
+            width="2"
+          ></v-progress-circular>
+          <template v-else>Send</template>
+        </v-btn>
+      </v-sheet>
+    </v-col>
+
+    <!-- text fields -->
+    <v-col cols="3">
+      <v-sheet class="pa-5" rounded="lg">
+        <h3 class="font-weight-regular text-h6 mb-2">
+          This is to test
+          <code>text fields</code>
+        </h3>
+        <p class="text-medium-emphasis mb-6">This is an example of using the custom text fields.</p>
+
+        <app-text-field
+          v-model="form.name"
+          :form="form"
+          name="name"
+          label="Your Name"
+          placeholder="Name"
+          required
+          class="mb-4"
+        ></app-text-field>
+
+        <app-text-field
+          v-model="form.email"
+          :form="form"
+          name="email"
+          label="Your Email"
+          placeholder="Email"
+          required
+          class="mb-4"
+        ></app-text-field>
+
+        <app-text-field
+          v-model="form.email"
+          :form="form"
+          name="email"
+          label="Your Email"
+          placeholder="Email"
+          required
+          class="mb-4"
+        ></app-text-field>
+
+        <app-text-field
+          v-model="form.password"
+          :form="form"
+          name="password"
+          type="password"
+          label="Your Password"
+          placeholder="Password"
+          required
+          class="mb-4"
+        ></app-text-field>
+
+        <v-btn
+          @click="sendRequest"
+          :disabled="form.busy"
+          size="large"
+          block
+          color="primary"
+          elevation="0"
+        >
+          <v-progress-circular
+            v-if="form.busy"
+            color="white"
+            bg-color="transparent"
+            indeterminate
+            size="27"
+            width="2"
+          ></v-progress-circular>
+          <template v-else>Send</template>
+        </v-btn>
+      </v-sheet>
+    </v-col>
   </v-row>
+
+  <app-dialog
+    ref="dialog"
+    title="Are you sure you want to delete this item?"
+    message="Are you sure you want to delete this item? this item will be deleted immediately. you cannot undo this action."
+  >
+    <template #actions="{ close, payload }">
+      <v-btn variant="flat" color="red" @click="close">Cancel</v-btn>
+      <v-btn variant="flat" color="primary" @click="deleteItem(payload)">I understand</v-btn>
+    </template>
+  </app-dialog>
 </template>
 
 <script setup>
-import AppDashboardHeading from '@/components/dashboard/Heading.vue'
+import { ref, reactive } from 'vue'
 import { useStore } from 'vuex'
+import { Form } from 'vform'
+import AppDialog from '@/components/common/Dialog.vue'
+import AppDashboardHeading from '@/components/dashboard/Heading.vue'
 
 const store = useStore()
+const dialog = ref(null)
+const form = reactive(
+  new Form({
+    name: 'Mustafa Omar',
+  })
+)
 
 function showToast() {
-  store.commit('app/notify', {
-    message: 'It is working perfectly fine!',
-    color: 'primary',
-  })
+  store.commit('app/notify', 'This is a message for primary toast!')
 }
 
 function showErrorToast() {
@@ -80,5 +253,29 @@ function showErrorToast() {
     message: 'This is a message for error toast.',
     color: 'red',
   })
+}
+
+function makeError() {
+  store.commit('app/error', {
+    message: 'Page not found',
+    type: 404,
+  })
+}
+
+function deleteItem(id) {
+  console.log(id)
+  dialog.value.hide()
+}
+
+function sendRequest() {
+  // prettier-ignore
+  form.post('/test')
+    .then(() => {})
+    .catch((error) => {
+      store.commit('app/notify', {
+        message: error.response.data.message,
+        color: 'red',
+      })
+    })
 }
 </script>
