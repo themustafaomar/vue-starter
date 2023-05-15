54173a092830142d0248bc8f3f1526e9613b8d9f
Mustafa Omar <themustafaomar@gmail.com>
2023-05-15 14:48:19 -0700

    chore: remove validation directory for now

--- src/plugins/index.js
+++ src/plugins/index.js
@@ -7,8 +7,9 @@ import { useVuetify } from './vuetify'
 import { useGlobalComponents } from './global-components'
 import { useLaravelPermissions } from './laravel-permissions'
 
-// Call the callalble plugins that needs
-// to the context parameters for registeration purposes.
+// Call the callalble plugins that needs to the app instance
+// for registeration purposes, since we're using Vue v3
+// we need the app instance to register components and plugins etc..
 export const plugins = (app) => {
   useVuetify(app)
   useLaravelPermissions(app)
--- src/validations/auth/register.js
+++
@@ -1,9 +0,0 @@
-import * as yup from 'yup'
-
-// prettier-ignore
-export const validationSchema = yup.object({
-  name: yup.string()
-    .min(5)
-    .required()
-    .label('Name'),
-})
