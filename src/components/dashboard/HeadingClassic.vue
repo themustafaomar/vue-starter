36f82255769f69246f254dadd212b4c84e6d8a95
Mustafa Omar <themustafaomar@gmail.com>
2023-06-28 18:46:32 +0300

    feat(components): heading classic component

---
+++ src/components/dashboard/HeadingClassic.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="d-flex align-center justify-space-between px-5 pb-5">
+    <div>
+      <h2 class="font-weight-medium text-h5 d-flex align-center text-grey-darken-3">
+        {{ title }}
+      </h2>
+    </div>
+    <slot name="actions"></slot>
+  </div>
+</template>
+
+<script setup>
+const props = defineProps({ title: String })
+</script>
